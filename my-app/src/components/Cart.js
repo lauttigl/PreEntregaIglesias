3c930b0ecf4ae39a90b6757ee22ae9e56316c542
lautaro iglesias <ljiglesias@outlook.com>
2022-12-28 11:25:36 -0300

    carrito funcional y algunos estilos

--- my-app/src/components/Cart.js
+++ my-app/src/components/Cart.js
@@ -14,17 +14,27 @@ export const Cart = () => {
 
 
     return (
-        <>
-        <div>Carrito de compras</div>
-        {items.map(item => <div key={item.id}>
-            <p>Producto: {item.name}</p>
-            <p>Cantidad: {item.quantity}</p>
-            <p>Precio: ${item.price}</p>
-            <p><button className='btn' onClick={() => removeItem(item.id)}> X </button></p>
-        </div>)}
-        <p>Total a pagar: ${total}</p>
-        <button className='btn'onClick={clearCart}>Vaciar Carrito</button>
-        </>
+        
+        <div className='border-2 gray-300 '>
+                <h1 className='text-2xl text-center '>Estas por comprar</h1>
+                {items.map(item => <div key={item.id} className=' p-2 text-center grid grid-cols-5 border-2 max-w-full'>
+                            <p className='col-start-1'>Producto: {item.name}</p>
+                            <p className='col-start-2'>Cantidad: {item.quantity}</p>
+                            <p className='col-start-3'>Precio: ${item.price}</p>
+                            <button className='btn btn-circle bg-red-600 rounded-md col-start-4' onClick={() => removeItem(item.id)}> X </button>
+                            </div>)}
+                            <div className='text-center'>
+                            <h3 className='text-2xl font-bold '>Total a pagar: ${total}</h3>
+                                <button className='btn bg-green-600 m-6 '>Comprar</button><button className='btn bg-red-600'onClick={clearCart}>Vaciar Carrito</button>
+                                </div>
+                                
+                                
+                            
+                    </div>
         
     )
     }
+
+
+
+    
--- my-app/src/components/Counter.js
+++ my-app/src/components/Counter.js
@@ -9,7 +9,16 @@ return (
     <button disabled={cuenta <=0}onClick={ () =>setCuenta (cuenta - 1)} className="btn"> - </button>
     <span className='text-lg'>{cuenta}</span>
     <button  disabled={cuenta >= stock}onClick={ () =>setCuenta (cuenta + 1)} className="btn btn-active"> + </button>
-    <p><button  disabled={cuenta == 0}  className="btn btn-active" onClick={() => onAdd(cuenta)} >Añadir al carrito</button></p>
+    <label htmlFor="my-modal" disabled={cuenta == 0} onClick={() => onAdd(cuenta)}  className="btn">Añadir al carrito</label>
+    <input type="checkbox" id="my-modal" className="modal-toggle" />
+    <div className="modal">
+    <div className="modal-box">
+    <h2 className="text-3xl text-center">El producto se agregó al carrito!</h2>
+    <div className="modal-action">
+    <label htmlFor="my-modal" className="btn">Seguir comprando</label>
+    </div>
+    </div>
+    </div>
     </div>
 )
 }
