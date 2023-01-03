a05c32993adc6c3b97c2f70749deb430335fa1ee
lautaro iglesias <ljiglesias@outlook.com>
2023-01-03 14:39:34 -0300

    agregue firebase a itemlistcontainer con imagenes

--- my-app/src/components/ItemListContainer.js
+++ my-app/src/components/ItemListContainer.js
@@ -70,7 +70,7 @@ setItemData(snapshot.docs.map(d => ({id:d.id, ...d.data()  } )))
                             <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-w-7 xl:aspect-h-8">
                             <img
                                 src={product.image}
-                                // alt={product.imageAlt}
+                                alt={product.imageAlt}
                                 className="h-full w-full object-cover object-center group-hover:opacity-75"
                             />
                             </div>
